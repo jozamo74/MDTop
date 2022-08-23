4df07c29b377ba48d3e3d73a414810115171c52d
José Zambrano <zambranomoya74@gmail.com>
2022-08-23 12:45:48 +0200

    Motion ContainerTransform

--- app/src/main/java/com/example/mdtop/MotionFragment.kt
+++ app/src/main/java/com/example/mdtop/MotionFragment.kt
@@ -1,9 +1,13 @@
 package com.example.mdtop
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.transition.TransitionManager
 import com.example.mdtop.databinding.FragmentMotionBinding
+import com.google.android.material.transition.MaterialArcMotion
+import com.google.android.material.transition.MaterialContainerTransform
 
 
 class MotionFragment : Fragment(R.layout.fragment_motion) {
@@ -13,6 +17,42 @@ class MotionFragment : Fragment(R.layout.fragment_motion) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMotionBinding.bind(view)
+
+        setupListener()
+    }
+
+    private fun setupListener() {
+
+        val transform = MaterialContainerTransform()
+        transform.scrimColor = Color.TRANSPARENT
+        transform.duration = 500L
+
+        binding.btnCancel.setOnClickListener {
+            with(binding) {
+
+                transform.startView = viewEnd
+                transform.endView = viewStart
+                transform.addTarget(viewStart)
+
+                TransitionManager.beginDelayedTransition(containerMain, transform)
+
+                viewStart.visibility = View.VISIBLE
+                viewEnd.visibility = View.GONE
+            }
+        }
+
+        binding.viewStart.setOnClickListener {
+            transform.setPathMotion(MaterialArcMotion())
+            transform.startView = binding.viewStart
+            transform.endView = binding.viewEnd
+            transform.addTarget(binding.viewEnd)
+
+            TransitionManager.beginDelayedTransition(binding.containerMain, transform)
+            with(binding){
+                viewStart.visibility = View.GONE
+                viewEnd.visibility = View.VISIBLE
+            }
+        }
     }
 
 }
